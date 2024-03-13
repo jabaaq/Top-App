d30b313aaa99eb02a54b7b5173127ad9a472892c
Jaba <jabaqadagishvili4@gmail.com>
2024-03-13 19:26:08 +0400

    created Button component to generate buttons / global styles are defined

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import { Htag } from "@/components";
+import { Button } from "@/components";
 
 export default function Home() {
   return (
     <div>
       <Htag tag="h1">Hello</Htag>
+      <Button appearance="primary">Button</Button>
+      <Button appearance="ghost">Button</Button>
     </div>
   );
 }
