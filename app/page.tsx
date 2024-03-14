669f3acb89851735e2e5fd57f1a7b953ab014070
Jaba <jabaqadagishvili4@gmail.com>
2024-03-14 22:24:37 +0400

    created a paragraph component to generate paragraphs

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
 import { Htag } from "@/components";
 import { Button } from "@/components";
+import { P } from "@/components";
 
 export default function Home() {
   return (
     <div>
       <Htag tag="h1">Hello</Htag>
-      <Button appearance="primary">Button</Button>
+      <Button appearance="primary" className="testClassname">
+        Button
+      </Button>
       <Button appearance="ghost">Button</Button>
+      <P size="s">Small</P>
+      <P size="m">Medium</P>
+      <P size="l">Large</P>
+      <P>Large</P>
     </div>
   );
 }
