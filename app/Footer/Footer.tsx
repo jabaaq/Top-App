4b434006307605c0fd9f75505c0bdc5c9f5a88b4
Jaba <jabaqadagishvili4@gmail.com>
2024-03-21 18:29:02 +0400

    the footer has been styled, it has become responsive

--- app/Footer/Footer.tsx
+++ app/Footer/Footer.tsx
@@ -1,7 +1,22 @@
+"use client";
 import { FooterProps } from "./Footer.props";
-import styles from "./Sidebar.module.css";
+import styles from "./Footer.module.css";
 import cn from "classNames";
+import { P } from "@/components";
+import { format } from "date-fns";
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
-  return <div {...props}>Footer</div>;
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+  return (
+    <div className={cn(className, styles.footer)} {...props}>
+      <div>
+        OwlTop © 2023 - {format(new Date(), "yyyy")} All rights reserved
+      </div>
+      <a href="#" target="_blank">
+        Terms of use
+      </a>
+      <a href="#" target="_blank">
+        Privacy Policy
+      </a>
+    </div>
+  );
 };
