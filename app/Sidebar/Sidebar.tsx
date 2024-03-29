0b51ea03471d8f31b617c15f7f5e955366ad877c
Jaba <jabaqadagishvili4@gmail.com>
2024-03-29 19:41:55 +0400

    created app context

--- app/Sidebar/Sidebar.tsx
+++ app/Sidebar/Sidebar.tsx
@@ -1,7 +1,15 @@
+"use client";
 import { SidebarProps } from "./Sidebar.props";
 import styles from "./Sidebar.module.css";
 import cn from "classNames";
+import { createContext } from "react";
+
+const SidebarContext = createContext({});
 
 export const Sidebar = ({ ...props }: SidebarProps): JSX.Element => {
-  return <div {...props}>Sidebar</div>;
+  return (
+    <SidebarContext.Provider value={{}}>
+      <div {...props}>Sidebar</div>
+    </SidebarContext.Provider>
+  );
 };
